e9e385adbf8abf7cb966cb3a3f1d7f04ec4cc12c
Omid Hassan <fastomid@gmail.com>
2020-10-05 09:25:08 -0400

    Created the wordlist xml file that stores the recipes

--- app/src/main/java/com/example/recyclerviewlab/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewlab/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.LinkedList;
 
+public class MainActivity extends AppCompatActivity {
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private final LinkedList<String> mDescriptions = new LinkedList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int x = 0; x < 20; x++)
+        {
+            mWordList.addLast("Word " + x);
+            mDescriptions.addLast("Word " + x);
+        }
+
     }
 }
